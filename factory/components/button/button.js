fcfb1b8261584b4541c5d579ec7caf43b6b33d5a
ccforeverd <zh1045456074@163.com>
2016-04-05 11:22:01 +0800

    修改首页

--- factory/components/button/button.js
+++ factory/components/button/button.js
@@ -2,37 +2,7 @@
 // H5微信端 --- component-button 按钮状态插件
 
 
-// define('h5-component-button', [], function() {});
-define(function() {
-
-	var format = function(string) { // 字符串格式化
-		// button-loading => buttonLoading
-		return string.split('-').reduce(function(str, item, index, array) {
-			if (index === 0) {
-				return item;
-			}
-			return str + item.replace(/^./, function(s) {
-				return s.toUpperCase();
-			});
-		}, '');
-	};
-
-	var Bind = function(name) {
-		// 属性
-		this.name = name; // keyboard 或 button-loading
-		this.handles = null;
-	};
-	// 类方法
-	Bind.format = format;
-	// 原型链方法
-	Bind.prototype.setHandles = function(json) {
-		bind.handles = json;
-	};
-	Bind.prototype.scan = function(context) {
-		$('[data-' + name + ']', context).each(function(i, element) {
-
-		});
-	};
-
-	return Bind;
+define('h5-component-button', ['bind'], function(Bind) {
+	var button = new Bind('button-loading');
+	return button;
 });
--- source/library/bind.js
+++ source/library/bind.js
@@ -2,7 +2,43 @@
 // H5微信端 --- 行间属性绑定方法
 
 
-define('bind', function() {
+define(function() {
 	
+	var format = function(string) { // 字符串格式化
+		// button-loading => buttonLoading
+		return string.split('-').reduce(function(str, item, index, array) {
+			if (index === 0) {
+				return item;
+			}
+			return str + item.replace(/^./, function(s) {
+				return s.toUpperCase();
+			});
+		}, '');
+	};
+
+	var Bind = function(name) {
+		// 属性
+		this.name = name; // keyboard 或 button-loading
+		this.handles = null;
+	};
+	// 类方法
+	Bind.format = format;
+	// 原型链方法
+	Bind.prototype.setHandles = function(json) {
+		bind.handles = json;
+	};
+	Bind.prototype.scan = function(context) {
+		$('[data-' + name + ']', context).each(function(i, element) {
+			element.dataset.keyboard.split('|').forEach(function(string) { //遍历属性上的方法
+				var match = string.match(/(\w+)(\(([\w\,\-]+)\))?/);
+				if (match && match[1] in handles) {
+					handles[match[1]].apply(element, match[3] ? match[3].split(',') : []);
+					//  handles[hide].apply(element , [contacts-search-input]);
+				}
+			});
+		}.bind(this));
+	};
+
+	return Bind;
 });
 
