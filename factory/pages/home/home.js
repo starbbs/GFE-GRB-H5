9e1d3fdee07d28ffc7ccea118a9685c7edb9e6ac
weibingbing5101 <510137831@qq.com>
2016-04-08 18:37:33 +0800

    2016年4月8日 18:37:32

--- factory/pages/home/home.js
+++ factory/pages/home/home.js
@@ -1,7 +1,7 @@
 // 张树垚 2015-12-20 11:27:22 创建
 // H5微信端 --- 个人首页
 (function() {
-	require(['router', 'h5-api', 'h5-price', 'h5-weixin', 'touch-slide'], function(router, api, price, weixin, TouchSlide) {
+	require(['router', 'h5-api', 'h5-price', 'h5-weixin', 'touch-slide', 'filters'], function(router, api, price, weixin, TouchSlide) {
 		router.init(true);
 		var gopToken = $.cookie('gopToken');
 		var main = $('.home');
