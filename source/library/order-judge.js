01bfc4f7ee03a8700aae524bd12a75a06f955b86
ccforeverd <zh1045456074@163.com>
2016-04-12 13:20:12 +0800

    更新订单进入判断

--- source/library/order-judge.js
+++ source/library/order-judge.js
@@ -5,26 +5,27 @@
 define('h5-order-judge', ['h5-api'], function(api) {
 	var gopToken = $.cookie('gopToken');
 	var res = {
-		check: function(curPrice,callback) {
+		check: function(curPrice, callback) {
 			var status = 'gopNumNo'; //状态果仁不够
 			var resultArr = [];
 			var todo = function() {
 				if (resultArr.length !== 2) {
 					return;
 				}
-				status = resultArr[0] * resultArr [1] > curPrice ? 'gopNumOk' : 'gopNumNo';
+				status = resultArr[0] * resultArr[1] > curPrice ? 'gopNumOk' : 'gopNumNo';
+				callback && callback(status, gopPrice, myGopNum);
 			};
 			var gopPrice = 0;
 			var myGopNum = 0;
 			//果仁现价
 			api.price({
-				gopToken:gopToken
-			},function(data){
-				if(data.status == '200'){
+				gopToken: gopToken
+			}, function(data) {
+				if (data.status == '200') {
 					resultArr.push(gopPrice = data.data.price);
 					todo();
 				}
-			});		
+			});
 			//获取果仁数
 			api.getGopNum({
 				gopToken: gopToken
