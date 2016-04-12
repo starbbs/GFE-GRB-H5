28cf2680f13838ebcc50e0f3ea6638ab6df76e63
weibingbing5101 <510137831@qq.com>
2016-04-12 15:49:59 +0800

    2016年4月12日 15:49:41
    discovery-temp调整位置

---
+++ factory/pages/discovery/discovery-temp.js
@@ -0,0 +1,19 @@
+// 张树垚 2016-02-24 16:16:44 创建
+// H5微信端 --- view-bill 账单详情分页
+
+
+define('h5-view-bill', [
+	'h5-api', 'router', 'filters', 'mydate', // 公用功能
+	'h5-view', 'h5-weixin', 'h5-component-bill', // H5功能
+	'h5-view-nickname', 'h5-dialog-confirm' // H5组件
+], function(
+	api, router, filters, mydate,
+	View, weixin, H5bill,
+	nicknameView, dialogConfirm
+) {
+
+	var gopToken = $.cookie('gopToken');
+
+
+
+});
