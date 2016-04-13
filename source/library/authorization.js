db12be25d740a29fcaa2325bb33ae0b1062c939c
ccforeverd <zh1045456074@163.com>
2016-04-13 14:15:25 +0800

    修改认证跳转

--- source/library/authorization.js
+++ source/library/authorization.js
@@ -3,12 +3,12 @@
 // H5微信端 --- 微信授权链接
 
 
-define('h5-authorization', ['get'], function(get) {
+define('h5-authorization', ['get', 'url'], function(get, url) {
 	// <a href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx55923db8dfb94e44&redirect_uri=http%3A%2F%2Fwww.goopal.me%2Findex2222.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect">点击授权登录</a>
 	var set = function(url) {
 		return 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx55923db8dfb94e44&redirect_uri=' + encodeURIComponent(url) + '&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
 	};
-	var url = 'https://www.goopal.com.cn/wx/';
+	var url = window.location.protocol + '//www.goopal.com.cn/wx/';
 	return {
 		// base: base,
 		set: set,
