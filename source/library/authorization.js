32b50eacefedb157ffd7baf6afb9dd5ee41270b2
weibingbing5101 <510137831@qq.com>
2016-04-21 16:28:47 +0800

    2016年4月21日 16:28:46

--- source/library/authorization.js
+++ source/library/authorization.js
@@ -5,6 +5,7 @@
 
 define('h5-authorization', ['get', 'url'], function(get, url) {
 	// <a href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx55923db8dfb94e44&redirect_uri=http%3A%2F%2Fwww.goopal.me%2Findex2222.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect">点击授权登录</a>
+	// 
 	var set = function(url, state) {
 		state = state || 'STATE';
 		return 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx55923db8dfb94e44&redirect_uri=' + encodeURIComponent(url) + '&response_type=code&scope=snsapi_userinfo&state=' + encodeURIComponent(state) + '#wechat_redirect';
