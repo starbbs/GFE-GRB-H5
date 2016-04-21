19ea539004c67c23d0138e172564ba38d43bce26
weibingbing5101 <510137831@qq.com>
2016-04-21 15:53:38 +0800

    sj

--- source/library/weixin.js
+++ source/library/weixin.js
@@ -128,7 +128,7 @@ define('h5-weixin', ['h5-api', 'url', 'h5-alert'], function(api, url) {
 			var returnData = data.data.signatureData;
 			wx.config({
 				debug: false, // 开启调试模式，调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-				appId: 'wxe91980c4944999fe', //weixin.appId = returnData.appId, // 必填，公众号的唯一标识
+				appId: weixin.appId = returnData.appId, // 必填，公众号的唯一标识
 				timestamp: weixin.timestamp = returnData.timestamp, // 必填，生成签名的时间戳
 				nonceStr: weixin.nonceStr = returnData.nonceStr, // 必填，生成签名的随机串
 				signature: weixin.signature = returnData.signature, // 必填，签名，见附录1
