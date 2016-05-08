3cba2b871a8963a61ec24e555ace4e844fb0f051
weibingbing5101 <510137831@qq.com>
2016-05-08 10:33:02 +0800

    2016年05月08日10:32:58

--- source/library/login-judge.js
+++ source/library/login-judge.js
@@ -11,20 +11,24 @@ define('h5-login-judge', ['h5-api', 'h5-authorization'], function(api, authoriza
 		};
 		var gopToken = $.cookie('gopToken');
 		if (gopToken) {
+			//alert('有TOKEN 去检查TOKEN');
 			api.getGopNum({
 				gopToken: gopToken
 			}, function(data) {
 				if (data.status == 200) {
+					//alert('TOKEN有效');
 					success && success();
 				} else if (data.status == 300) { //增加如果用户锁定进冻结页面
 					setTimeout(function() {
 						window.location.href = './frozen.html?type=locked'
 					}, 210);
 				} else {
+					//alert('TOKEN无效 去授权');
 					jumpOut();
 				}
 			});
 		} else {
+			//alert('没有TOKEN 去授权');
 			jumpOut();
 		}
 	};
