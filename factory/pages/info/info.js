119a1c0ef10ea5949523e0a93782599667ca8db0
余效俭 <ijieshu@163.com>
2016-05-08 18:12:32 +0800

    bug修复

--- factory/pages/info/info.js
+++ factory/pages/info/info.js
@@ -37,34 +37,33 @@ require([
 
 	//get方法返回一个json｛data:'',parse:'',stringify:'',add:''｝
 	//get.data = {from:wx_info,type:BUYIN_ORDER,id:1111}
-	if(!gopToken){
-		switch (get.data.from) {
-		case 'wx_info': // 来自微信消息
-			switch (type) {
-				case 'CONSUME_ORDER': // 超时关闭消费果仁订单消息
-				case 'BUYIN_ORDER': // 超时关闭买果仁订单消息
-				case 'TRANSFER_IN': // 转入消息
-				case 'TRANSFER_OUT': // 转出消息
-				case 'REFUND': // 退款
-					// window.location.href = 'transfer.html?from=wx_info&id=' + get.data.id;
-					billView.set(type, get.data.id, {
-						onRendered: function() {
-							router.to('/bill');
-						},
-					});
-					break;
-				case 'DUOBAO_ACTIVITY': // 果仁夺宝
-					gotoHome('果仁夺宝尚未上线!');
-					break;
-				default:
-					gotoHome('不是正确的微信消息种类');
-					break;
-			}
-			break;
-		default:
-			gotoHome('没有收到通知');
-			break;
+
+	switch (get.data.from) {
+	case 'wx_info': // 来自微信消息
+		switch (type) {
+			case 'CONSUME_ORDER': // 超时关闭消费果仁订单消息
+			case 'BUYIN_ORDER': // 超时关闭买果仁订单消息
+			case 'TRANSFER_IN': // 转入消息
+			case 'TRANSFER_OUT': // 转出消息
+			case 'REFUND': // 退款
+				// window.location.href = 'transfer.html?from=wx_info&id=' + get.data.id;
+				billView.set(type, get.data.id, {
+					onRendered: function() {
+						router.to('/bill');
+					},
+				});
+				break;
+			case 'DUOBAO_ACTIVITY': // 果仁夺宝
+				gotoHome('果仁夺宝尚未上线!');
+				break;
+			default:
+				gotoHome('不是正确的微信消息种类');
+				break;
 		}
+		break;
+	default:
+		gotoHome('没有收到通知');
+		break;
 	}
-	
+
 });
