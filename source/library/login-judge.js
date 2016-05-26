a95c8e2703a4fb043228c22a42da9841d5b46462
gaoerjun <gaoerjun@new4g.com>
2016-05-26 09:41:56 +0800

    完善接口

--- source/library/login-judge.js
+++ source/library/login-judge.js
@@ -28,6 +28,8 @@ define('h5-login-judge', ['h5-api', 'h5-authorization'], function(api, authoriza
 					}else{
 						jumpOut();
 					}
+				}else{
+					jumpOut();
 				}
 			})
 			// api.getGopNum({
