c05fdee437c3620f3e37559cad2ac3f53c57b375
weibingbing5101 <510137831@qq.com>
2016-05-30 10:23:26 +0800

    版本号修改

--- factory/views/agreement/agreement.js
+++ factory/views/agreement/agreement.js
@@ -1,4 +1,3 @@
-
 // 张树垚 2016-01-21 15:28:29 创建
 // H5微信端 --- view-agreement 关于我们
 
@@ -6,6 +5,6 @@
 define('h5-view-agreement', ['h5-view', 'get'], function(View, get) {
 	var agreement = new View('agreement');
 	// agreement.v = $('#js-v').html('V' + (get.data.v || '1.0'));
-	get.data.app === 'ios' && get.data.v === '1.2.0' && $('.js-hide').remove();
+	// get.data.app === 'ios' && get.data.v === '1.2.0' && $('.js-hide').remove();
 	return agreement;
 });
