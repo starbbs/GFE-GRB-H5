25fc41a4e4b50c1d39c40df1109c87adf4c3cdd9
gaoerjun <gaoerjun@new4g.cn>
2016-06-04 13:57:19 +0800

    完善统计

--- source/library/param-config.js
+++ source/library/param-config.js
@@ -9,7 +9,8 @@ define('h5-config', function() {
 	var paramConfig = {
 		baseUri : '//172.16.33.3:18080',
 		main : '//www.xiaojian.me/wx/',
-		appid : 'wxe91980c4944999fe'
+		appid : 'wxe91980c4944999fe',
+		countAPIDomain:"//172.16.33.10:8089"
 	}
 	return paramConfig;
 });
