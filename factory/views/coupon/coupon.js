752aee1e3598bb6dc1806e1276927a26876ed49f
weibingbing5101 <510137831@qq.com>
2016-06-04 14:21:15 +0800

    去除多余

--- factory/views/coupon/coupon.js
+++ factory/views/coupon/coupon.js
@@ -1,12 +1,18 @@
-// 张树垚 2016-02-24 16:16:44 创建
+// 魏冰冰 2016-05-27        创建
 // H5微信端 --- view-coupon 优惠券分页
-
+/**
+ * [set 设置账单详情]
+ * @Author   魏冰冰
+ * @DateTime 2016-03-09
+ * @param    {[function]}     		couponListView.vm.onClickFn		[优惠券列表点击后的回调]
+ * @param    {[function]}     		set								[按页面URL设置优惠券列表]
+ */
 
 define('h5-view-coupon', ['h5-api', 'router', 'get', 'url', 'h5-view', 'h5-weixin'], function(api, router, get, url, View) {
 	var gopToken = $.cookie('gopToken');
 
-	var canuse = [];  //可用优惠券数组
-	var disuse = [];  //不可用优惠券数组
+	var canuse = []; //可用优惠券数组
+	var disuse = []; //不可用优惠券数组
 	var couponListView = new View('coupon-list');
 	var couponDetailView = new View('coupon-detail');
 	var couponDetailJSON = { //清空详情的数据
@@ -38,57 +44,57 @@ define('h5-view-coupon', ['h5-api', 'router', 'get', 'url', 'h5-view', 'h5-weixi
 	}, couponDetailJSON));
 
 	avalon.scan();
-	
+
 	//日期处理，结束日期展示时需要提前一天
-	var getPreDay = function(str){
-		var strDate = new Date(str.replace(/-/g,"/"));  //字符串转日期
-		var newDate = new Date(strDate - 24*60*60*1000); //日期减一天操作
-		var newDateStr = newDate.getFullYear()+"-"+(newDate.getMonth()+1)+"-"+newDate.getDate(); //获取新的日期
+	var getPreDay = function(str) {
+		var strDate = new Date(str.replace(/-/g, "/")); //字符串转日期
+		var newDate = new Date(strDate - 24 * 60 * 60 * 1000); //日期减一天操作
+		var newDateStr = newDate.getFullYear() + "-" + (newDate.getMonth() + 1) + "-" + newDate.getDate(); //获取新的日期
 		return newDateStr;
 	}
-	
-    var dataHandler = function(arr,type){
-    	var nowTime = new Date().getTime();
-        arr.forEach(function(item,index,array){
-            var itemStartTime = new Date(item.startTime.replace(/-/g,"/")).getTime();
-            var itemEndTime =  new Date(item.endTime.replace(/-/g,"/")).getTime();
-            item.startTime = item.startTime.substr(0,10);
-            item.endTime = getPreDay(item.endTime.substr(0,10));  //对当前结束时间进行建议减一天的操作
-            item.disuse = false;
-            if(nowTime > itemEndTime){
-                item.voucherStatus = 'EXPIRE';
-                disuse.push(item);
-            }else{
-                item.voucherStatus = 'AVAILABLE';
-                canuse.push(item);
-            }
-        });
-        if(type === 'order'){
-            canuse.forEach(function(item,index,array){
-                var itemStartTime = new Date(item.startTime).getTime();
-                var itemEndTime =  new Date(item.endTime).getTime();
-                if(nowTime < itemStartTime){
-                	item.disuse = true;
-                    item.voucherStatus = 'EXPIRE';
-                    disuse.push(item);
-                    canuse.splice(index,1,'');
-                }
-            });
-            canuse = canuse.filter(function(item,index,array){
-                return item != '';
-            });
-        }
-    }
+
+	var dataHandler = function(arr, type) {
+		var nowTime = new Date().getTime();
+		arr.forEach(function(item, index, array) {
+			var itemStartTime = new Date(item.startTime.replace(/-/g, "/")).getTime();
+			var itemEndTime = new Date(item.endTime.replace(/-/g, "/")).getTime();
+			item.startTime = item.startTime.substr(0, 10);
+			item.endTime = getPreDay(item.endTime.substr(0, 10)); //对当前结束时间进行建议减一天的操作
+			item.disuse = false;
+			if (nowTime > itemEndTime) {
+				item.voucherStatus = 'EXPIRE';
+				disuse.push(item);
+			} else {
+				item.voucherStatus = 'AVAILABLE';
+				canuse.push(item);
+			}
+		});
+		if (type === 'order') {
+			canuse.forEach(function(item, index, array) {
+				var itemStartTime = new Date(item.startTime).getTime();
+				var itemEndTime = new Date(item.endTime).getTime();
+				if (nowTime < itemStartTime) {
+					item.disuse = true;
+					item.voucherStatus = 'EXPIRE';
+					disuse.push(item);
+					canuse.splice(index, 1, '');
+				}
+			});
+			canuse = canuse.filter(function(item, index, array) {
+				return item != '';
+			});
+		}
+	}
 
 	//我的   券列表处理
 	var mineHandler = function(arr) {
 		//详情消失 重新渲染数据
 		//数据处理函数
-		dataHandler(arr,'mine');
-        $.extend(couponListView.VM,{
-            listAva:canuse,
-            listExp:disuse    
-        })
+		dataHandler(arr, 'mine');
+		$.extend(couponListView.VM, {
+			listAva: canuse,
+			listExp: disuse
+		})
 		couponListView.on('show', function() {
 			$.extend(couponDetailView.VM, couponDetailJSON);
 		});
@@ -97,11 +103,11 @@ define('h5-view-coupon', ['h5-api', 'router', 'get', 'url', 'h5-view', 'h5-weixi
 	//定单  券列表处理
 	var orderHandler = function(arr) {
 		//数据处理函数
-		dataHandler(arr,'order');
-        $.extend(couponListView.VM,{
-            listAva:canuse,
-            listExp:disuse    
-        })
+		dataHandler(arr, 'order');
+		$.extend(couponListView.VM, {
+			listAva: canuse,
+			listExp: disuse
+		})
 
 	};
 
--- factory/views/discovery-temp/discovery-temp.js
+++
@@ -1,7 +0,0 @@
-// 张树垚 2016-02-24 16:16:44 创建
-// H5微信端 --- view-bill 账单详情分页
-
-
-define('h5-view-bill', [], function() {
-	var gopToken = $.cookie('gopToken');
-});
