aedeccce6395334363ccf4085d2dfcc6285b0ca5
jiangxiaoni <jiangxiaoni321@gmail.com>
2016-07-26 19:57:37 +0800

    切换到api接口请求数据

--- factory/pages/experience-gop/experience-gop.js
+++ factory/pages/experience-gop/experience-gop.js
@@ -5,120 +5,126 @@
 require(['router', 'h5-api', 'h5-weixin','filters','h5-dialog-confirm','h5-alert',],function(
 	router, api, weixin, filters, dialogConfirm
 ){
-	var list={
-	    "data":{
-	        "list":[
-	            {
-	                "createTime":"2016-07-22 09:29:33",
-	                "getGopPrice":7.05,
-	                "gopPrice":15.959793,
-	                "expireDate":"2016-07-29 00:00:00",
-	                "validDays":3,
-	                "updateTime":"2016-07-22 15:39:27",
-	                "id":1,
-	                "gopNum":1500,
-	                "userId":63,
-	                "getGopNum":0.70922,
-	                "status":"PROCESSING"
-	            },
-	            {
-	                "createTime":"2016-07-22 09:29:33",
-	                "getGopPrice":7.05,
-	                "gopPrice":17,
-	                "expireDate":"2016-07-29 00:00:00",
-	                "validDays":1,
-	                "updateTime":"2016-07-22 15:39:27",
-	                "id":3,
-	                "gopNum":1500,
-	                "userId":63,
-	                "getGopNum":0.70922,
-	                "status":"PROCESSING"
-	            },
-	            {
-	                "createTime":"2016-07-22 09:29:33",
-	                "getGopPrice":7.05,
-	                "gopPrice":16,
-	                "expireDate":"2016-07-29 00:00:00",
-	                "validDays":2,
-	                "updateTime":"2016-07-22 15:39:27",
-	                "id":4,
-	                "gopNum":1500,
-	                "userId":63,
-	                "getGopNum":0.70922,
-	                "status":"PROCESSING"
-	            },
-	            {
-	                "createTime":"2016-07-22 09:29:33",
-	                "getGopPrice":7.05,
-	                "gopPrice":15.959789,
-	                "expireDate":"2016-07-29 00:00:00",
-	                "validDays":2,
-	                "updateTime":"2016-07-22 15:39:27",
-	                "id":42,
-	                "gopNum":1500,
-	                "userId":63,
-	                "getGopNum":0.70922,
-	                "status":"PROCESSING"
-	            },
-	            {
-	                "createTime":"2016-07-22 09:29:33",
-	                "getGopPrice":7.05,
-	                "gopPrice":0.01,
-	                "expireDate":"2016-07-29 00:00:00",
-	                "validDays":1,
-	                "updateTime":"2016-07-22 15:39:27",
-	                "id":5,
-	                "gopNum":1500,
-	                "userId":63,
-	                "getGopNum":0.70922,
-	                "status":"LOCKED"
-	            },
-	            {
-	                "createTime":"2016-07-22 09:29:33",
-	                "getGopPrice":7.05,
-	                "gopPrice":0.01,
-	                "expireDate":"2016-07-29 00:00:00",
-	                "validDays":0,
-	                "updateTime":"2016-07-22 15:39:27",
-	                "id":9,
-	                "gopNum":1500,
-	                "userId":63,
-	                "getGopNum":0.70922,
-	                "status":"LOCKED"
-	            },
-	            {
-	                "createTime":"2016-07-22 09:32:33",
-	                "getGopPrice":7,
-	                "gopPrice":7,
-	                "expireDate":"2016-07-29 00:00:00",
-	                "validDays":2,
-	                "updateTime":"2016-07-22 10:03:26",
-	                "id":2,
-	                "gopNum":500,
-	                "userId":63,
-	                "getGopNum":0.71429,
-	                "status":"WITHDRAW"
-	            }
-	            ,
-	            {
-	                "createTime":"2016-07-22 09:32:33",
-	                "getGopPrice":7,
-	                "gopPrice":17,
-	                "expireDate":"2016-07-29 00:00:00",
-	                "validDays":0,
-	                "updateTime":"2016-07-22 10:03:26",
-	                "id":80,
-	                "gopNum":500,
-	                "userId":63,
-	                "getGopNum":0.71429,
-	                "status":"PROCESSING"
-	            }
-	        ],
-	        "minIncome":1
-	    },
-	    "msg":"success",
-	    "status":"200"
-	}
+//	api.obtain({
+//		gopToken:"9bfcd178472f48bcb28584dc4ed82a04",
+//  		toUserId:"143"
+//	},function(data){
+//		alert(data.status);
+//	});
+//	var list={
+//	    "data":{
+//	        "list":[
+//	            {
+//	                "createTime":"2016-07-22 09:29:33",
+//	                "getGopPrice":7.05,
+//	                "gopPrice":15.959793,
+//	                "expireDate":"2016-07-29 00:00:00",
+//	                "validDays":3,
+//	                "updateTime":"2016-07-22 15:39:27",
+//	                "id":1,
+//	                "gopNum":1500,
+//	                "userId":63,
+//	                "getGopNum":0.70922,
+//	                "status":"PROCESSING"
+//	            },
+//	            {
+//	                "createTime":"2016-07-22 09:29:33",
+//	                "getGopPrice":7.05,
+//	                "gopPrice":17,
+//	                "expireDate":"2016-07-29 00:00:00",
+//	                "validDays":1,
+//	                "updateTime":"2016-07-22 15:39:27",
+//	                "id":3,
+//	                "gopNum":1500,
+//	                "userId":63,
+//	                "getGopNum":0.70922,
+//	                "status":"PROCESSING"
+//	            },
+//	            {
+//	                "createTime":"2016-07-22 09:29:33",
+//	                "getGopPrice":7.05,
+//	                "gopPrice":16,
+//	                "expireDate":"2016-07-29 00:00:00",
+//	                "validDays":2,
+//	                "updateTime":"2016-07-22 15:39:27",
+//	                "id":4,
+//	                "gopNum":1500,
+//	                "userId":63,
+//	                "getGopNum":0.70922,
+//	                "status":"PROCESSING"
+//	            },
+//	            {
+//	                "createTime":"2016-07-22 09:29:33",
+//	                "getGopPrice":7.05,
+//	                "gopPrice":15.959789,
+//	                "expireDate":"2016-07-29 00:00:00",
+//	                "validDays":2,
+//	                "updateTime":"2016-07-22 15:39:27",
+//	                "id":42,
+//	                "gopNum":1500,
+//	                "userId":63,
+//	                "getGopNum":0.70922,
+//	                "status":"PROCESSING"
+//	            },
+//	            {
+//	                "createTime":"2016-07-22 09:29:33",
+//	                "getGopPrice":7.05,
+//	                "gopPrice":0.01,
+//	                "expireDate":"2016-07-29 00:00:00",
+//	                "validDays":1,
+//	                "updateTime":"2016-07-22 15:39:27",
+//	                "id":5,
+//	                "gopNum":1500,
+//	                "userId":63,
+//	                "getGopNum":0.70922,
+//	                "status":"LOCKED"
+//	            },
+//	            {
+//	                "createTime":"2016-07-22 09:29:33",
+//	                "getGopPrice":7.05,
+//	                "gopPrice":0.01,
+//	                "expireDate":"2016-07-29 00:00:00",
+//	                "validDays":0,
+//	                "updateTime":"2016-07-22 15:39:27",
+//	                "id":9,
+//	                "gopNum":1500,
+//	                "userId":63,
+//	                "getGopNum":0.70922,
+//	                "status":"LOCKED"
+//	            },
+//	            {
+//	                "createTime":"2016-07-22 09:32:33",
+//	                "getGopPrice":7,
+//	                "gopPrice":7,
+//	                "expireDate":"2016-07-29 00:00:00",
+//	                "validDays":2,
+//	                "updateTime":"2016-07-22 10:03:26",
+//	                "id":2,
+//	                "gopNum":500,
+//	                "userId":63,
+//	                "getGopNum":0.71429,
+//	                "status":"WITHDRAW"
+//	            }
+//	            ,
+//	            {
+//	                "createTime":"2016-07-22 09:32:33",
+//	                "getGopPrice":7,
+//	                "gopPrice":17,
+//	                "expireDate":"2016-07-29 00:00:00",
+//	                "validDays":0,
+//	                "updateTime":"2016-07-22 10:03:26",
+//	                "id":80,
+//	                "gopNum":500,
+//	                "userId":63,
+//	                "getGopNum":0.71429,
+//	                "status":"PROCESSING"
+//	            }
+//	        ],
+//	        "minIncome":1
+//	    },
+//	    "msg":"success",
+//	    "status":"200"
+//	}
 	router.init(true);
 	var gopToken = $.cookie('gopToken');
 	var experienceVM = avalon.define({
@@ -160,74 +166,72 @@ require(['router', 'h5-api', 'h5-weixin','filters','h5-dialog-confirm','h5-alert
 			getData();
 		});
 	}
-	getSellOnePrice();
+	
 	//获取后台返回的体验果仁list
 	var getData = function(){
-		list.data.list && list.data.list.forEach(function(item){
-		if(item.status!="WITHDRAW"){
-			//收益
-			item.income = item.status === 'LOCKED' ? item.getGopPrice*item.getGopNum : (experienceVM.gopNowPrice-item.gopPrice)*item.gopNum;
-			if(item.validDays < 1 && item.income < 0){
-				item.income = list.data.minIncome;
-			}
-			//判断条件
-			item.flag1 = item.validDays>=1;
-			item.flag = (experienceVM.gopNowPrice-item.gopPrice) < 0 && item.validDays > 0;
-			item.flag2 = (experienceVM.gopNowPrice-item.gopPrice) == 0 && item.validDays > 0;
-			//收益小数部分计算
-			var deci = Math.abs(item.income)-Math.abs(parseInt(item.income));
-			item.gopDecimal = filters.ceilFix(deci,2).split(".")[1];
-			//收益为负 且 已到期 时收益小数部分
-			var minDeci = filters.ceilFix((Math.abs(list.data.minIncome)-Math.abs(parseInt(list.data.minIncome))),2).split(".")[1];
-			//+ -
-			item.sign = (experienceVM.gopNowPrice-item.gopPrice) >= 0 ? '+' : (item.validDays >= 1 ? '-' : '+');
-			//收益整数、小数部分显示
-			item.incomeInt = item.validDays >= 1 ? (item.income > 0 ? parseInt(item.income) : Math.abs(parseInt(item.income))) : (item.income > 1 ? parseInt(item.income) : parseInt(list.data.minIncome));
-			//alert(item.income);
-			item.incomeDec = item.validDays >= 1 ? "."+item.gopDecimal : (item.income > 1 ? "."+item.gopDecimal : "."+minDeci);
-			//获取价保留两位小数
-			item.gopPrice=filters.ceilFix(item.gopPrice,2);
-			item.desc = !item.flag1 ? "已到期":(item.status =='LOCKED'?"涨停":"");		
-
-			experienceVM.experienceList.push(item);
-		}
-	})
+		 api.experienceGopList({gopToken:gopToken},function(data){
+			 	if(data.status == 200){
+			 		//experienceList=data.data.list;			
+			 		data.data.list && data.data.list.forEach(function(item){
+			 			if(item.status!="WITHDRAW"){
+			 				//收益
+			 				item.income=(experienceVM.gopNowPrice-item.gopPrice)*item.gopNum;
+			 				item.flag = (experienceVM.gopNowPrice-item.gopPrice) < 0 && item.validDays > 0;
+			 				//收益小数部分计算
+			 				var deci = Math.abs(item.income)-Math.abs(parseInt(item.income));
+			 				item.gopDecimal = filters.ceilFix(deci,2).split(".")[1];
+			 				//收益为负 且 已到期 时收益小数部分
+			 				var minDeci = filters.ceilFix((Math.abs(data.data.minIncome)-Math.abs(parseInt(data.data.minIncome))),2).split(".")[1];
+			 				//+ -
+			 				item.sign = (experienceVM.gopNowPrice-item.gopPrice) >= 0 ? '+' : (item.validDays >= 1 ? '-' : '+');
+			 				//收益整数、小数部分显示
+			 				item.incomeInt = item.validDays >= 1 ? (item.income > 0 ? parseInt(item.income) : Math.abs(parseInt(item.income))) : (item.income > 1 ? parseInt(item.income) : parseInt(data.data.minIncome));
+			 				//alert(item.income);
+			 				item.incomeDec = item.validDays >= 1 ? "."+item.gopDecimal : (item.income > 1 ? "."+item.gopDecimal : "."+minDeci);
+			 				//获取价保留两位小数
+			 				item.gopPrice=filters.ceilFix(item.gopPrice,2);
+			 				experienceVM.experienceList.push(item);
+			 			}
+			 			if(item.status == "LOCKED"){
+			 				item.incomeInt = item.validDays >= 1 ? "涨停" : (item.income > 1 ? parseInt(item.income) : parseInt(data.data.minIncome));
+			 				item.incomeDec = item.validDays >= 1 ? "" : (item.income > 1 ? "."+item.gopDecimal : "."+minDeci);
+			 			}
+			 		})
+			 	}else{
+			 		$.alert(data.msg);
+			 	}
+			 })
+//			list.data.list && list.data.list.forEach(function(item){
+//			if(item.status!="WITHDRAW"){
+//				//收益
+//				item.income = item.status === 'LOCKED' ? item.getGopPrice*item.getGopNum : (experienceVM.gopNowPrice-item.gopPrice)*item.gopNum;
+//				if(item.validDays < 1 && item.income < 0){
+//					item.income = list.data.minIncome;
+//				}
+//				//判断条件
+//				item.flag1 = item.validDays>=1;
+//				item.flag = (experienceVM.gopNowPrice-item.gopPrice) < 0 && item.validDays > 0;
+//				item.flag2 = (experienceVM.gopNowPrice-item.gopPrice) == 0 && item.validDays > 0;
+//				//收益小数部分计算
+//				var deci = Math.abs(item.income)-Math.abs(parseInt(item.income));
+//				item.gopDecimal = filters.ceilFix(deci,2).split(".")[1];
+//				//收益为负 且 已到期 时收益小数部分
+//				var minDeci = filters.ceilFix((Math.abs(list.data.minIncome)-Math.abs(parseInt(list.data.minIncome))),2).split(".")[1];
+//				//+ -
+//				item.sign = (experienceVM.gopNowPrice-item.gopPrice) >= 0 ? '+' : (item.validDays >= 1 ? '-' : '+');
+//				//收益整数、小数部分显示
+//				item.incomeInt = item.validDays >= 1 ? (item.income > 0 ? parseInt(item.income) : Math.abs(parseInt(item.income))) : (item.income > 1 ? parseInt(item.income) : parseInt(list.data.minIncome));
+//				//alert(item.income);
+//				item.incomeDec = item.validDays >= 1 ? "."+item.gopDecimal : (item.income > 1 ? "."+item.gopDecimal : "."+minDeci);
+//				//获取价保留两位小数
+//				item.gopPrice=filters.ceilFix(item.gopPrice,2);
+//				item.desc = !item.flag1 ? "已到期":(item.status =='LOCKED'?"涨停":"");		
+//	
+//				experienceVM.experienceList.push(item);
+//			}
+//		})
 	}
+	getSellOnePrice();
 	
-
-
-	//有接口后使用，不删！！！！
-	// api.experienceGopList({gopToken:gopToken},function(data){
-	// 	if(data.status == 200){
-	// 		//experienceList=data.data.list;			
-	// 		data.data.list && data.data.list.forEach(function(item){
-	// 			if(item.status!="WITHDRAW"){
-	// 				//收益
-	// 				item.income=(experienceVM.gopNowPrice-item.gopPrice)*item.gopNum;
-	// 				item.flag = (experienceVM.gopNowPrice-item.gopPrice) < 0 && item.validDays > 0;
-	// 				//收益小数部分计算
-	// 				var deci = Math.abs(item.income)-Math.abs(parseInt(item.income));
-	// 				item.gopDecimal = filters.ceilFix(deci,2).split(".")[1];
-	// 				//收益为负 且 已到期 时收益小数部分
-	// 				var minDeci = filters.ceilFix((Math.abs(data.data.minIncome)-Math.abs(parseInt(data.data.minIncome))),2).split(".")[1];
-	// 				//+ -
-	// 				item.sign = (experienceVM.gopNowPrice-item.gopPrice) >= 0 ? '+' : (item.validDays >= 1 ? '-' : '+');
-	// 				//收益整数、小数部分显示
-	// 				item.incomeInt = item.validDays >= 1 ? (item.income > 0 ? parseInt(item.income) : Math.abs(parseInt(item.income))) : (item.income > 1 ? parseInt(item.income) : parseInt(data.data.minIncome));
-	// 				//alert(item.income);
-	// 				item.incomeDec = item.validDays >= 1 ? "."+item.gopDecimal : (item.income > 1 ? "."+item.gopDecimal : "."+minDeci);
-	// 				//获取价保留两位小数
-	// 				item.gopPrice=filters.ceilFix(item.gopPrice,2);
-	// 				experienceVM.experienceList.push(item);
-	// 			}
-	// 			if(item.status == "LOCKED"){
-	// 				item.incomeInt = item.validDays >= 1 ? "涨停" : (item.income > 1 ? parseInt(item.income) : parseInt(data.data.minIncome));
-	// 				item.incomeDec = item.validDays >= 1 ? "" : (item.income > 1 ? "."+item.gopDecimal : "."+minDeci);
-	// 			}
-	// 		})
-	// 	}else{
-	// 		$.alert(data.msg);
-	// 	}
-	// })
 	avalon.scan();	
 });
