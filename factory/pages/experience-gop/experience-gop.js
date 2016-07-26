00bf49bc84c5d4f4526b01cd446b90fcee078673
zhouyuan <pinkhide-zy@qq.com>
2016-07-26 16:08:40 +0800

    tiyanguoren js

--- factory/pages/experience-gop/experience-gop.js
+++ factory/pages/experience-gop/experience-gop.js
@@ -10,6 +10,7 @@ require(['router', 'h5-api', 'h5-weixin','filters','h5-dialog-confirm','h5-alert
 	var experienceVM = avalon.define({
 		$id: 'experience',
 		gopNowPrice : 0, //果仁现价（取卖一价）
+		experienceList:[],
 		drawConfirm:function(){
 			var gopId=$(this).parents(".screen-r-middle-menu-li").get(0).dataset.id;
 			var getGopNum=$(this).parents(".screen-r-middle-menu-li").get(0).dataset.gopnum;
@@ -60,25 +61,27 @@ require(['router', 'h5-api', 'h5-weixin','filters','h5-dialog-confirm','h5-alert
 	//有接口后使用，不删！！！！
 	api.experienceGopList({gopToken:gopToken},function(data){
 		if(data.status==200){
-			//experienceList=data.data.list;
-			experienceList=[];
+			//experienceList=data.data.list;			
 			data.data.list && data.data.list.forEach(function(item){
-				//if(item.status!="WITHDRAW"){
+				if(item.status!="WITHDRAW"){
 					item.flag = (item.getGopPrice-item.gopPrice) < 0 && item.validDays > 0;
+					//收益
+					item.income=(item.getGopPrice-item.gopPrice)*item.gopNum;
 					//收益小数部分计算
-					var deci = Math.abs((item.getGopPrice-item.gopPrice)*item.gopNum)-Math.abs(parseInt((item.getGopPrice-item.gopPrice)*item.gopNum));
+					var deci = Math.abs(item.income)-Math.abs(parseInt(item.income));
 					item.gopDecimal = filters.ceilFix(deci,2).split(".")[1];
 					//收益为负 且 已到期 时收益小数部分
 					var minDeci = filters.ceilFix((Math.abs(data.data.minIncome)-Math.abs(parseInt(data.data.minIncome))),2).split(".")[1];
 					//+ -
 					item.sign = item.getGopPrice-item.gopPrice >= 0 ? '+' : (item.validDays >= 1 ? '-' : '+');
 					//收益整数、小数部分显示
-					item.incomeInt = (item.getGopPrice-item.gopPrice)*item.gopNum > 0 ? parseInt((item.getGopPrice-item.gopPrice)*item.gopNum) : (item.validDays >=1 ? Math.abs(parseInt((item.getGopPrice-item.gopPrice)*item.gopNum)) : parseInt(list.data.minIncome));
+					item.incomeInt = item.validDays >= 1 ? (item.income > 0 ? parseInt(item.income) : Math.abs(parseInt(item.income))) : (item.income > 1 ? parseInt(item.income) : parseInt(data.data.minIncome));
+					//alert(item.income);
+					item.incomeDec = item.validDays >= 1 ? item.gopDecimal : (item.income > 1 ? item.gopDecimal : minDeci);
 					//获取价保留两位小数
 					item.gopPrice=filters.ceilFix(item.gopPrice,2);
-					item.incomeDec = (item.getGopPrice-item.gopPrice)*item.gopNum > 0 ? item.gopDecimal : (item.validDays >= 1 ? item.gopDecimal : minDeci);
-					experienceList.push(item);
-				//}
+					experienceVM.experienceList.push(item);
+				}
 			})
 		}else{
 			$.alert(data.msg);
