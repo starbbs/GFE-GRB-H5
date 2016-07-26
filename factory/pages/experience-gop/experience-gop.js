852e30910323c596e127c145e7dc8dbfb3201176
linqiaojuan <linqiaojuan@new4g.cn>
2016-07-26 13:40:31 +0800

    加入接口

--- factory/pages/experience-gop/experience-gop.js
+++ factory/pages/experience-gop/experience-gop.js
@@ -39,7 +39,18 @@ require(['router', 'h5-api', 'h5-weixin','filters','h5-dialog-confirm'],function
 		drawConfirm:function(){
 			 dialogConfirm.set('<div class="screen-r-popup"> <div class="screen-r-popup-top"> 现在领取收益 <span class="screen-r-popup-top-f"> 1.08</span> 个果仁（价值 <span class="screen-r-popup-top-f"> 15.35</span>元）将会进入您的账户中，同时您的体验果仁将会被系统回收 </div><div class="screen-r-popup-bottom">确定领取？</div> </div>', {okBtnText: '确定', cancelBtnText: "取消"});
 			dialogConfirm.show();
+			//以下是确定事件！！！！！
 			dialogConfirm.onConfirm = function () {
+				api.experienceGopWithdraw({
+				    "gopToken":"7ea593562e3547e792985f6884f793d6",
+				    "exeprienceGopId":"2"
+				},function(data){
+					if(data.status==200){
+						
+					}else{
+						$.alert(data.msg);
+					}
+				});
 			};	
 		}
 	});
