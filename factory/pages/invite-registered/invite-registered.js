53a2518b7d23392aa4fdecf55fa3bdd6061ac002
linqiaojuan <linqiaojuan@new4g.cn>
2016-08-02 15:24:38 +0800

    进入分享页判断和分享规则页面跳转

--- factory/pages/invite-registered/invite-registered.js
+++ factory/pages/invite-registered/invite-registered.js
@@ -2,24 +2,63 @@
 // H5微信端 --- 体验果仁
 
 require(['router', 'h5-api', 'h5-weixin', 'filters', 'h5-view', 'h5-alert', ], function(
-	router, api, weixin, filters, View
+	router, api, weixin, filters, View, h5alert
 ) {
+//		a={
+//  "data": {
+//      "max": 2,
+//      "limit": false
+//  },
+//  "msg": "success",
+//  "status": "200"
+//}
 	router.init(true);
+	new View('invite-kefu');
 	var gopToken = $.cookie('gopToken');
 	var inventeVM = avalon.define({
 		$id: 'invente',
-		click : function(){
-			api.aaa({
-				gopToken : gopToken
-			},function(data){
-				if(data.status === 200){
-					flag = true;
-				}else{
-					router.go("/invite-kefu.html");
+		mideFlag: false, //定义详细规则页面开始是隐藏
+		shareFlag: false,
+		//		定义点击详细规则后页面跳出
+		minuteRule: function() {
+			inventeVM.mideFlag = true;
+		},
+		//		点击×号后关闭页面
+		mideClose: function() {
+			inventeVM.mideFlag = false;
+		},
+		//点击立即邀请好友判断邀请数是否超过上限
+		minuteInvente: function() {
+			//调接口
+			api.inviteFriendLimit({
+				gopToken: gopToken
+			}, function(data) {
+				if(data.status === 200) {
+					if(data.data.limit) {
+						router.go("/invite-kefu");//limit为true时跳转到二维码页面
+					} else {
+						inventeVM.shareFlag = true;
+					}
+				} else {
+					$.alert(data.msg);
 				}
 			});
-		}
-	aaa();
-	$(".screen-reg").addClass('on');
-	avalon.scan();	
-});
+		},
+//		minuteInvente: function() {
+//			router.go("/invite-kefu");
+//		},
+//minuteInvente: function() {
+//	if(a.data.limit) {
+//		router.go("/invite-kefu");
+//	} else {
+//		inventeVM.shareFlag = true;
+//	}
+//},
+
+		
+	});
+	
+	$(".screen-popup ").addClass('on'); //延迟
+	$(".screen-share").addClass('on'); //延迟
+	avalon.scan();
+})
