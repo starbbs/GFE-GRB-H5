f5b309655bec494f9a17b262135cab534a43ca5d
jiangxiaoni <jiangxiaoni321@gmail.com>
2016-08-03 14:38:59 +0800

    体验果仁的果仁数改为三位小数

--- factory/pages/experience-gop/experience-gop.js
+++ factory/pages/experience-gop/experience-gop.js
@@ -20,7 +20,7 @@ require(['router', 'h5-api', 'h5-weixin','filters','h5-dialog-confirm','mathtool
 				api.getselloneprice(function(data) {
 					experienceVM.gopNowPrice = data.optimumBuyPrice;
 					var gopId = _this.id;
-					var gopNum = filters.floorFix(_this.income / experienceVM.gopNowPrice,2);
+					var gopNum = filters.floorFix(_this.income / experienceVM.gopNowPrice,3);
 					var income = _this.income; //领取时的收益
 					dialogConfirm.set('<div class="screen-r-popup"> <div class="screen-r-popup-top"> 现在领取收益 <span class="screen-r-popup-top-f"> '+gopNum+'</span> 个果仁（价值 <span class="screen-r-popup-top-f">'+income+'</span>元）将会进入您的账户中，同时您的体验果仁将会被系统回收 </div><div class="screen-r-popup-bottom">确定领取？</div></div>', {okBtnText: '确定', cancelBtnText: "取消"});
 					dialogConfirm.show();
